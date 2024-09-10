e07acc816c883735fc70c2df7c7a74ea1822b2ec
LeonardoBloom <leonardocolacogg@gmail.com>
2024-09-10 19:15:49 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("WELCOME to the guessing game !!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess. ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please type a number goddammit !!!");
+
+        println!("You guessed: {guess}");
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Guessed too low"),
+            Ordering::Greater => println!("Guessed too high"),
+            Ordering::Equal => println!("You win!"),
+        }
+    }
+
+}
